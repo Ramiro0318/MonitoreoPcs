0db60aa726c3ef1f0c7a02130576606addd3ad63
Ramiro0318 <ramiro.val03@gmail.com>
2026-05-02 17:09:19 -0600

    Ajuste para la persistencia de ultimo ping y ultimo latido para la lista de computadoras historicas.

--- Servidor/Services/ServerService.cs
+++ Servidor/Services/ServerService.cs
@@ -307,17 +307,19 @@ namespace Servidor.Services
                 if (DateTime.Now - pc.UltimoLatido >= TimeSpan.FromSeconds(30) && pc.EstadoEnlazado)
                 {
                     pc.EstadoEnlazado = false;
+                    GuardarOC(Computadoras, computadorasFilename);
                     ComputadoraEnlazada?.Invoke(pc);
                 }
                 if (DateTime.Now - pc.UltimoPing >= TimeSpan.FromSeconds(30) && pc.EstadoInternet)
                 {
                     pc.EstadoInternet = false;
+                    GuardarOC(Computadoras, computadorasFilename);
                     ComputadoraEnlazada?.Invoke(pc);
                 }
-                if ((DateTime.Now - pc.UltimoLatido >= TimeSpan.FromHours(1) && DateTime.Now - pc.UltimoPing >= TimeSpan.FromHours(1) && !pc.EstadoHistorico))
+                if ((DateTime.Now - pc.UltimoLatido >= TimeSpan.FromHours(1) && DateTime.Now - pc.UltimoPing >= TimeSpan.FromHours(1)) && !pc.EstadoHistorico)
                 {
                     pc.EstadoHistorico = true;
-
+                    GuardarOC(Computadoras, computadorasFilename);
                 }
             }
         }
